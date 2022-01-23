bc7d6956004a9b64cf28accbf1599b0ee812c41c
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-23 18:21:23 +0300

    This commit will test save user account

--- locker_test.py
+++ locker_test.py
@@ -10,9 +10,17 @@ class TestClass(unittest.TestCase):
 
         self.new_user = User('Ness','kokokrim')
     
-    #test 
+    #test user account
     def test_init(self):
 
         self.assertEqual(self.new_user.username,'Ness')
         self.assertEqual(self.new_user.password,'kokokrim')
+
+
+    # test save user account
+
+    def test_save_user(self):
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
