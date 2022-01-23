43a66e1c837d121b7933b39ac21454cc98b055ea
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-23 18:58:07 +0300

    This commit will add locker function

--- runlocker.py
+++ runlocker.py
@@ -3,7 +3,7 @@ from locker import User, Credentials
 
 
 #graphics
-def function():
+def graphics():
 	print("              ___          ____       ______     _      _               _______                   ")
 	print("              |  |       /     \    /   ____|   |  |  /  /              |   _   \                 ")
 	print("              |  |      |   --   |  |   |       |  | /  /     _____     |  |_|   |                ")
@@ -12,7 +12,7 @@ def function():
 	print("              |_______|  \______/    \______|   |_ |  \__\              | _|   \__\               ")
 print("                                                                                                      ")                                         
 
-function()
+graphics()
 
 
 #create account function
@@ -90,3 +90,163 @@ def generate_Password():
 def copy_password(account):
 
     return Credentials.copy_password(account)
+
+
+
+def locker():
+    
+    #welcome message
+    print(" Hello, Welcome to Lock-R, A program built to store user credentials ...\n \n Please enter one of the following to proceed.\n CA ---  Sign in  \n LI ---  Have an account, Log in  \n")
+    short_code=input("").lower().strip()
+
+    if short_code == "ca":
+
+        print("Fill in the details below")
+
+        print('*' * 170)
+        username = input("User_name: ")
+        print('*' * 170)
+
+        while True:
+            print(" IP - To input own pasword:\n GP - To generate random Password")
+            password_Choice = input().lower().strip()
+
+            if password_Choice == 'ip':
+                password = input("Enter Password\n")
+                break
+
+            elif password_Choice == 'gp':
+                password = generate_Password()
+                break
+
+            else:
+                print("Wrong password, please try again")
+
+        #to save new user Lock-R account        
+        save_user(create_new_user(username,password))
+        print("*"*85)
+        print(f"Dear {username}, Your account has been created succesfully! Your password is: {password}")
+        print("*"*85)
+        
+    #Log in Lock-R account    
+    elif short_code == "li":
+
+        print(" ")
+        print("*"*50)
+        print("Enter Lock-R Username and Password to Log in:")
+        print('*' * 50)
+        print(" ")
+
+       #verifying log in
+        username = input("Username: ")
+        password = input("password: ")
+        login = login_user(username,password)
+
+        if login_user == login:
+            print(f"Hello {username}.Welcome back to Lock-R!")  
+            print('\n')
+
+    while True:
+        print("Use these codes:\n CC - New Credential \n VC - Display Credentials \n EC - look for existing credential \n GP - Generate A randomn password \n Del - Delete credential \n EX - Logout \n")
+        short_code = input().lower().strip()
+        
+        #new credentials
+        if short_code == "cc":
+            print("Create New Credential")
+            print("."*20)
+            print("Application name:  (e.g 'Instagram')")
+            account = input().lower()
+            print("Your Account username")
+            userName = input()
+
+            while True:
+                print(" TP - To type your own password: \n GP - To let Lock-R generate random Password for you")
+                password_Choice = input().lower().strip()
+
+                if password_Choice == 'tp':
+                    password = input("Enter Your Own Password \n")
+                    break
+
+                elif password_Choice == 'gp':
+                    password = generate_Password()
+                    break
+
+                else:
+                    print("Invalid password please try again")
+
+            #save new credential
+            save_credentials(create_new_credential(account,userName,password))
+            print('\n') 
+            print(f"Account Credential for:  {account} -  UserName:  {userName}  -  Password:  {password}  created and saved succesfully!")
+            print('\n')
+        
+
+        #display/ view credentials
+        elif short_code == "vc":
+
+            if display_accounts_details():
+                print("Here's your list of saved Credentials: ")
+                 
+                print('*' * 35)
+                print('_'* 35)
+
+                for account in display_accounts_details():
+                    print(f" Account:{account.account} \n User Name:{username}\n Password:{password}")
+                    print('_'* 35)
+                print('*' * 35)
+                
+            else:
+                print("No existing credentials saved yet...")
+       
+        
+
+        #look for existing credentials using application name
+        elif short_code == "ec":
+            print("Enter the Application Name you want to search for")
+            search_name = input().lower()
+
+            if find_credential(search_name):
+                search_credential = find_credential(search_name)
+                print(f"Account Name : {search_credential.account}")
+                print('-' * 50)
+                print(f"User Name: {search_credential.userName} Password :{search_credential.password}")
+                print('-' * 50)
+
+            else:
+                print("That Credential does not exist")
+                print('\n')
+        
+
+        #delete credentials
+        elif short_code == "del":
+            print("Enter the Application name of the Credentials you want to delete")
+            search_name = input().lower()
+            
+            if find_credential(search_name):
+                search_credential = find_credential(search_name)
+                print("_"*50)
+                search_credential.delete_credentials()
+                print('\n')
+                print(f"Your credentials for : {search_credential.account} is successfully deleted!!!")
+                print('\n')
+
+            else:
+                print(" Credential to be deleted does not exist")
+         
+        #generate random password
+        elif short_code == 'gp':
+
+            password = generate_Password()
+            print(f" succesfully generated {password} ")
+
+        #log out of Lock-R account
+        elif short_code == 'ex':
+            print("Thank you for using Lock-R.. See you next time!")
+            break
+
+        else:
+            print("Wrong entry! let entry match those in menu")
+    
+    
+if __name__ == '__main__':
+    locker()
