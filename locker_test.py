c1e846436cfd218956c4e538135934e7de08c506
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-23 18:11:51 +0300

    This commit will add test class and set up.

---
+++ locker_test.py
@@ -0,0 +1,14 @@
+import unittest
+from locker import User
+from locker import Credentials
+
+#test class
+class TestClass(unittest.TestCase):
+    
+    #function that sets up test class
+    def setUp(self):
+
+        self.new_user = User('Ness','kokokrim')
+
+    
+
